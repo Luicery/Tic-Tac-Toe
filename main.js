4acd240c134012db230232d684aa15dba86e2066
Luicery <56919859+Luicery@users.noreply.github.com>
2019-12-10 14:16:16 +1100

    Game X and O working but not scoring

---
+++ main.js
@@ -0,0 +1,202 @@
+$(document).ready(function() {
+  let currentPlayer = "X";
+  let squareAmount = 9;
+  $("#1").on("click", function() {
+    if($("#1").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#1").attr("src", "LetterX.png")
+        $("#1").attr("width", 150)
+        $("#1").attr("height", 150)
+        $("#1").attr("padding", "0px")
+        $("#1").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#1").attr("src", "Circle.png")
+        $("#1").attr("width", 150)
+        $("#1").attr("height", 150)
+        $("#1").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#2").on("click", function() {
+    if($("#2").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#2").attr("src", "LetterX.png")
+        $("#2").attr("width", 150)
+        $("#2").attr("height", 150)
+        $("#2").attr("padding", "0px")
+        $("#2").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#2").attr("src", "Circle.png")
+        $("#2").attr("width", 150)
+        $("#2").attr("height", 150)
+        $("#2").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#3").on("click", function() {
+    if($("#3").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#3").attr("src", "LetterX.png")
+        $("#3").attr("width", 150)
+        $("#3").attr("height", 150)
+        $("#3").attr("padding", "0px")
+        $("#3").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#3").attr("src", "Circle.png")
+        $("#3").attr("width", 150)
+        $("#3").attr("height", 150)
+        $("#3").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#4").on("click", function() {
+    if($("#4").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#4").attr("src", "LetterX.png")
+        $("#4").attr("width", 150)
+        $("#4").attr("height", 150)
+        $("#4").attr("padding", "0px")
+        $("#4").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#4").attr("src", "Circle.png")
+        $("#4").attr("width", 150)
+        $("#4").attr("height", 150)
+        $("#4").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#5").on("click", function() {
+    if($("#5").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#5").attr("src", "LetterX.png")
+        $("#5").attr("width", 150)
+        $("#5").attr("height", 150)
+        $("#5").attr("padding", "0px")
+        $("#5").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#5").attr("src", "Circle.png")
+        $("#5").attr("width", 150)
+        $("#5").attr("height", 150)
+        $("#5").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#6").on("click", function() {
+    if($("#6").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#6").attr("src", "LetterX.png")
+        $("#6").attr("width", 150)
+        $("#6").attr("height", 150)
+        $("#6").attr("padding", "0px")
+        $("#6").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#6").attr("src", "Circle.png")
+        $("#6").attr("width", 150)
+        $("#6").attr("height", 150)
+        $("#6").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#7").on("click", function() {
+    if($("#7").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#7").attr("src", "LetterX.png")
+        $("#7").attr("width", 150)
+        $("#7").attr("height", 150)
+        $("#7").attr("padding", "0px")
+        $("#7").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#7").attr("src", "Circle.png")
+        $("#7").attr("width", 150)
+        $("#7").attr("height", 150)
+        $("#7").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#8").on("click", function() {
+    if($("#8").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#8").attr("src", "LetterX.png")
+        $("#8").attr("width", 150)
+        $("#8").attr("height", 150)
+        $("#8").attr("padding", "0px")
+        $("#8").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#8").attr("src", "Circle.png")
+        $("#8").attr("width", 150)
+        $("#8").attr("height", 150)
+        $("#8").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+  $("#9").on("click", function() {
+    if($("#9").attr("src") === "") {
+      if(currentPlayer === "X") {
+        $("#9").attr("src", "LetterX.png")
+        $("#9").attr("width", 150)
+        $("#9").attr("height", 150)
+        $("#9").attr("padding", "0px")
+        $("#9").css({
+          "padding":"0px"
+        })
+        currentPlayer = "O"
+      } else {
+        $("#9").attr("src", "Circle.png")
+        $("#9").attr("width", 150)
+        $("#9").attr("height", 150)
+        $("#9").css({
+          "padding":"0px"
+        })
+        currentPlayer = "X"
+      }
+    }
+  })
+})
