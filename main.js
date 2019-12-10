336d4fa4a80e8cb413df600d4d387f6b4a599009
Luicery <56919859+Luicery@users.noreply.github.com>
2019-12-10 18:57:06 +1100

    Shortened and optimised code for Javascript functions

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   let squareAmount = 9;
   let Player1ScoreAmount = 0;
   let Player2ScoreAmount = 0;
+  let currentSquare = 0
   let boardClear = function() {
     for (i = 1; i <= squareAmount; i++) {
       $("#"+i).attr("src","");
@@ -22,32 +23,12 @@ $(document).ready(function() {
     };
   };
   let checkWinner = function() {
-    if(($("#1").attr("src") === "LetterX.png" && $("#2").attr("src") === "LetterX.png" && $("#3").attr("src") === "LetterX.png") || ($("#4").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#6").attr("src") === "LetterX.png") || ($("#7").attr("src") === "LetterX.png" && $("#8").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png")) {
+    if(($("#1").attr("src") === "LetterX.png" && $("#2").attr("src") === "LetterX.png" && $("#3").attr("src") === "LetterX.png") || ($("#4").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#6").attr("src") === "LetterX.png") || ($("#7").attr("src") === "LetterX.png" && $("#8").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png") || ($("#1").attr("src") === "LetterX.png" && $("#4").attr("src") === "LetterX.png" && $("#7").attr("src") === "LetterX.png") || ($("#2").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#8").attr("src") === "LetterX.png") || ($("#3").attr("src") === "LetterX.png" && $("#6").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png") || ($("#1").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png") || ($("#3").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#7").attr("src") === "LetterX.png")) {
       console.log("Player 1 wins");
       Player1ScoreAmount = Player1ScoreAmount + 1;
       $("#Player1Score").text(Player1ScoreAmount);
       boardClear();
-    } else if (($("#1").attr("src") === "LetterX.png" && $("#4").attr("src") === "LetterX.png" && $("#7").attr("src") === "LetterX.png") || ($("#2").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#8").attr("src") === "LetterX.png") || ($("#3").attr("src") === "LetterX.png" && $("#6").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png")) {
-      console.log("Player 1 wins");
-      Player1ScoreAmount = Player1ScoreAmount + 1;
-      $("#Player1Score").text(Player1ScoreAmount);
-      boardClear();
-    } else if (($("#1").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#9").attr("src") === "LetterX.png") || ($("#3").attr("src") === "LetterX.png" && $("#5").attr("src") === "LetterX.png" && $("#7").attr("src") === "LetterX.png")) {
-      console.log("Player 1 wins");
-      Player1ScoreAmount = Player1ScoreAmount + 1;
-      $("#Player1Score").text(Player1ScoreAmount);
-      boardClear();
-    } else if(($("#1").attr("src") === "Circle.png" && $("#2").attr("src") === "Circle.png" && $("#3").attr("src") === "Circle.png") || ($("#4").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#6").attr("src") === "Circle.png") || ($("#7").attr("src") === "Circle.png" && $("#8").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png")) {
-      console.log("Player 2 wins");
-      Player2ScoreAmount = Player2ScoreAmount + 1;
-      $("#Player2Score").text(Player2ScoreAmount);
-      boardClear();
-    } else if (($("#1").attr("src") === "Circle.png" && $("#4").attr("src") === "Circle.png" && $("#7").attr("src") === "Circle.png") || ($("#2").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#8").attr("src") === "Circle.png") || ($("#3").attr("src") === "Circle.png" && $("#6").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png")) {
-      console.log("Player 2 wins");
-      Player2ScoreAmount = Player2ScoreAmount + 1;
-      $("#Player2Score").text(Player2ScoreAmount);
-      boardClear();
-    } else if (($("#1").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png") || ($("#3").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#7").attr("src") === "Circle.png")) {
+    } else if(($("#1").attr("src") === "Circle.png" && $("#2").attr("src") === "Circle.png" && $("#3").attr("src") === "Circle.png") || ($("#4").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#6").attr("src") === "Circle.png") || ($("#7").attr("src") === "Circle.png" && $("#8").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png") || ($("#1").attr("src") === "Circle.png" && $("#4").attr("src") === "Circle.png" && $("#7").attr("src") === "Circle.png") || ($("#2").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#8").attr("src") === "Circle.png") || ($("#3").attr("src") === "Circle.png" && $("#6").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png") || ($("#1").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#9").attr("src") === "Circle.png") || ($("#3").attr("src") === "Circle.png" && $("#5").attr("src") === "Circle.png" && $("#7").attr("src") === "Circle.png")) {
       console.log("Player 2 wins");
       Player2ScoreAmount = Player2ScoreAmount + 1;
       $("#Player2Score").text(Player2ScoreAmount);
@@ -57,207 +38,23 @@ $(document).ready(function() {
       boardClear();
     };
   };
-  $("#1").on("click", function() {
-    if($("#1").attr("src") === "") {
+  $(".square").on("click", function() {
+    if($("#" + this.id).attr("src") === "") {
       if(currentPlayer === "X") {
-        $("#1").attr("src", "LetterX.png");
-        $("#1").attr("width", 150);
-        $("#1").attr("height", 150);
-        $("#1").css({
+        $("#" + this.id).attr("src", "LetterX.png");
+        $("#" + this.id).attr("width", 150);
+        $("#" + this.id).attr("height", 150);
+        $("#" + this.id).css({
           "padding":"0px"
         });
         currentPlayer = "O";
       } else {
-        $("#1").attr("src", "Circle.png");
-        $("#1").attr("width", 150);
-        $("#1").attr("height", 150);
-        $("#1").css({
+        $("#" + this.id).attr("src", "Circle.png");
+        $("#" + this.id).attr("width", 150);
+        $("#" + this.id).attr("height", 150);
+        $("#" + this.id).css({
           "padding":"0px"
         })
-        currentPlayer = "X";
-      };
-    };
-    checkWinner();
-    changeTurns();
-  });
-  $("#2").on("click", function() {
-    if($("#2").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#2").attr("src", "LetterX.png");
-        $("#2").attr("width", 150);
-        $("#2").attr("height", 150);
-        $("#2").css({
-          "padding":"0px"
-        });
-        currentPlayer = "O";
-      } else {
-        $("#2").attr("src", "Circle.png");
-        $("#2").attr("width", 150);
-        $("#2").attr("height", 150);
-        $("#2").css({
-          "padding":"0px"
-        });
-        currentPlayer = "X";
-      };
-    };
-    checkWinner();
-    changeTurns();
-  });
-  $("#3").on("click", function() {
-    if($("#3").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#3").attr("src", "LetterX.png");
-        $("#3").attr("width", 150);
-        $("#3").attr("height", 150);
-        $("#3").css({
-          "padding":"0px"
-        });
-        currentPlayer = "O";
-      } else {
-        $("#3").attr("src", "Circle.png");
-        $("#3").attr("width", 150);
-        $("#3").attr("height", 150);
-        $("#3").css({
-          "padding":"0px"
-        })
-        currentPlayer = "X";
-      }
-    }
-    checkWinner();
-    changeTurns();
-  })
-  $("#4").on("click", function() {
-    if($("#4").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#4").attr("src", "LetterX.png");
-        $("#4").attr("width", 150);
-        $("#4").attr("height", 150);
-        $("#4").css({
-          "padding":"0px"
-        })
-        currentPlayer = "O";
-      } else {
-        $("#4").attr("src", "Circle.png");
-        $("#4").attr("width", 150);
-        $("#4").attr("height", 150);
-        $("#4").css({
-          "padding":"0px"
-        })
-        currentPlayer = "X";
-      }
-    }
-    checkWinner();
-    changeTurns();
-  })
-  $("#5").on("click", function() {
-    if($("#5").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#5").attr("src", "LetterX.png");
-        $("#5").attr("width", 150);
-        $("#5").attr("height", 150);
-        $("#5").css({
-          "padding":"0px"
-        })
-        currentPlayer = "O";
-      } else {
-        $("#5").attr("src", "Circle.png");
-        $("#5").attr("width", 150);
-        $("#5").attr("height", 150);
-        $("#5").css({
-          "padding":"0px"
-        })
-        currentPlayer = "X";
-      }
-    }
-    checkWinner();
-    changeTurns();
-  })
-  $("#6").on("click", function() {
-    if($("#6").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#6").attr("src", "LetterX.png");
-        $("#6").attr("width", 150);
-        $("#6").attr("height", 150);
-        $("#6").css({
-          "padding":"0px"
-        })
-        currentPlayer = "O";
-      } else {
-        $("#6").attr("src", "Circle.png");
-        $("#6").attr("width", 150);
-        $("#6").attr("height", 150);
-        $("#6").css({
-          "padding":"0px"
-        })
-        currentPlayer = "X";
-      }
-    }
-    checkWinner();
-    changeTurns();
-  })
-  $("#7").on("click", function() {
-    if($("#7").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#7").attr("src", "LetterX.png");
-        $("#7").attr("width", 150);
-        $("#7").attr("height", 150);
-        $("#7").css({
-          "padding":"0px"
-        })
-        currentPlayer = "O";
-      } else {
-        $("#7").attr("src", "Circle.png");
-        $("#7").attr("width", 150);
-        $("#7").attr("height", 150);
-        $("#7").css({
-          "padding":"0px"
-        })
-        currentPlayer = "X";
-      }
-    }
-    checkWinner();
-    changeTurns();
-  })
-  $("#8").on("click", function() {
-    if($("#8").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#8").attr("src", "LetterX.png");
-        $("#8").attr("width", 150);
-        $("#8").attr("height", 150);
-        $("#8").css({
-          "padding":"0px"
-        })
-        currentPlayer = "O";
-      } else {
-        $("#8").attr("src", "Circle.png");
-        $("#8").attr("width", 150);
-        $("#8").attr("height", 150);
-        $("#8").css({
-          "padding":"0px"
-        });
-        currentPlayer = "X";
-      };
-    };
-    checkWinner();
-    changeTurns();
-  });
-  $("#9").on("click", function() {
-    if($("#9").attr("src") === "") {
-      if(currentPlayer === "X") {
-        $("#9").attr("src", "LetterX.png");
-        $("#9").attr("width", 150);
-        $("#9").attr("height", 150);
-        $("#9").css({
-          "padding":"0px"
-        });
-        currentPlayer = "O";
-      } else {
-        $("#9").attr("src", "Circle.png");
-        $("#9").attr("width", 150);
-        $("#9").attr("height", 150);
-        $("#9").css({
-          "padding":"0px"
-        });
         currentPlayer = "X";
       };
     };
